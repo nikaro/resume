95818aee37727d20bc280e037a0f5ae9627ee216
Nicolas Karolak <nicolas@karolak.fr>
2021-01-16 15:15:22 +0100

    update

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,198 +2,208 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Nicolas Karolak'
-SITENAME = 'Nicolas Karolak'
-SITEURL = ''
-SITEDESCRIPTION = ''
+AUTHOR = "Nicolas Karolak"
+SITENAME = "Nicolas Karolak"
+SITEURL = ""
+SITEDESCRIPTION = ""
 
-PATH = 'content'
+PATH = "content"
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = "Europe/Paris"
 
-DEFAULT_LANG = 'fr'
+DEFAULT_LANG = "fr"
 
 FEED_ALL_ATOM = None
 
 RELATIVE_URLS = True
 
-THEME = 'resume'
-CSS_FILE = 'main-6.css'
+THEME = "resume"
+CSS_FILE = "main-6.css"
 
 # Profile information
-NAME = 'Nicolas Karolak'
-TAGLINE = 'Ingénieur Systèmes Linux'
-PIC = '../../images/photo.jpg'
+NAME = "Nicolas Karolak"
+TAGLINE = "Ingénieur Systèmes Linux"
+PIC = "../../images/photo.jpg"
 
 # sidebar links
-EMAIL = 'nicolas@karolak.fr'
-PHONE = '+33651422138'
-WEBSITE = 'blog.karolak.fr'
-LINKEDIN = 'nicolas-karolak'
-GITHUB = 'nikaro'
-SOURCEHUT = 'nka'
+EMAIL = "nicolas@karolak.fr"
+PHONE = "+33651422138"
+WEBSITE = "blog.karolak.fr"
+LINKEDIN = "nicolas-karolak"
+GITHUB = "nikaro"
+SOURCEHUT = "nka"
 TWITTER = None
 
-DOWNLOAD_PDF = 'images/cv-nicolas-karolak.pdf'
+DOWNLOAD_PDF = "images/cv-nicolas-karolak.pdf"
 
 CAREER_SUMMARY = "Passionné d'informatique. Orienté Linux et automatisation. Amateur de logiciels libres et open source. Contributeur occasionnel. Télétravailleur. J'ai besoin que mon travail ait du sens et une utilité sociale."
 
 SKILLS = [
     {
-        'title': 'Linux : Debian, CentOS, ArchLinux',
-        'level': '80',
-        },
+        "title": "Linux : Debian, CentOS, ArchLinux",
+        "level": "80",
+    },
     {
-        'title': 'IaC : Ansible, Git, Gitlab CI, Terraform',
-        'level': '90',
-        },
+        "title": "IaC : Ansible, Git, Gitlab CI, Terraform",
+        "level": "90",
+    },
     {
-        'title': 'Web : NGINX, HAProxy, Apache2',
-        'level': '80',
-        },
+        "title": "Web : NGINX, HAProxy, Apache2",
+        "level": "80",
+    },
     {
-        'title': 'Virtualisation : LXD/LXC, KVM, Docker',
-        'level': '70',
-        },
+        "title": "Virtualisation : LXD/LXC, KVM, Docker",
+        "level": "70",
+    },
     {
-        'title': 'Cloud : AWS, Scaleway, OVH',
-        'level': '50',
-        },
+        "title": "Cloud : AWS, Scaleway, OVH",
+        "level": "50",
+    },
     {
-        'title': 'Scripting : Bash, Python, PHP, Golang',
-        'level': '80',
-        },
+        "title": "Scripting : Bash, Python, PHP, Golang",
+        "level": "80",
+    },
     {
-        'title': 'Réseaux : Pare-feu, Routage, VLAN',
-        'level': '60',
-        },
-    ]
+        "title": "Réseaux : Pare-feu, Routage, VLAN",
+        "level": "60",
+    },
+]
 
 PROJECTS = [
     {
-        'title': 'Contributions',
-        'tagline': 'Plusieurs soumissions dans quelques projets Open Source, tels que Ansible, Pyinfra, Rss2email, Rss-Bridge, et de nombreux rapports de bogues.',
-        },
+        "title": "Contributions",
+        "tagline": "Plusieurs soumissions dans quelques projets Open Source, tels que Ansible, Pyinfra, Rss2email, Rss-Bridge, et de nombreux rapports de bogues.",
+    },
     {
-        'title': 'Devc',
-        'link': 'https://git.sr.ht/~nka/devc',
-        'tagline': "Outil de gestion de devcontainer en CLI, écrit en Go. Un devcontainer permet d'isoler les outils de l'environnement de développement dans un conteneur Docker en lien avec l'éditeur de code.",
-        },
+        "title": "Devc",
+        "link": "https://git.sr.ht/~nka/devc",
+        "tagline": "Outil de gestion de devcontainer en CLI, écrit en Go. Un devcontainer permet d'isoler les outils de l'environnement de développement dans un conteneur Docker en lien avec l'éditeur de code.",
+    },
     {
-        'title': 'Infra',
-        'link': 'https://git.sr.ht/~nka/infra',
-        'tagline': "Dépôt contenant les scripts de déploiement et d'orchestration de mon infrastructure personnelle (serveur de messagerie, web, Nextcloud, etc.), en Ansible et Terraform.",
-        },
-    ]
+        "title": "Infra",
+        "link": "https://git.sr.ht/~nka/infra",
+        "tagline": "Dépôt contenant les scripts de déploiement et d'orchestration de mon infrastructure personnelle (serveur de messagerie, web, Nextcloud, etc.), en Ansible et Terraform.",
+    },
+]
 
 LANGUAGES = [
     {
-        'name': 'Français',
-        'description': 'Courant',
-        },
+        "name": "Français",
+        "description": "Courant",
+    },
     {
-        'name': 'Anglais',
-        'description': 'Professionnel',
-        },
-    ]
+        "name": "Anglais",
+        "description": "Professionnel",
+    },
+]
 
 INTERESTS = [
-    'Informatique',
-    'Judo',
-    'Lecture',
-    'Cuisine',
-    'Cinéma',
-    'Restaurant',
-    'Randonnée',
-    'Cueillette de champignons',
-    'Moto',
-    ]
+    "Informatique",
+    "Judo",
+    "Lecture",
+    "Cuisine",
+    "Cinéma",
+    "Restaurant",
+    "Randonnée",
+    "Cueillette de champignons",
+    "Moto",
+]
 
 EXPERIENCES = [
     {
-        'job_title': 'Ingénieur Systèmes Linux',
-        'time': "Mai 2020 - Aujourd'hui",
-        'company': 'Université Gustave Eiffel, Champs-sur-Marne',
-        'details': [
-            'Déploiement de services (WireGuard, pfSense, etc.)',
+        "job_title": "Administrateur Systèmes Linux",
+        "time": "Octobre 2020 - Aujourd'hui",
+        "company": "Université Gustave Eiffel, Champs-sur-Marne",
+        "details": [
+            "Mise en place cluster GlusterFS",
+            "Écriture de tests de charge avec Locust",
+            "Scripting Bash et Ansible",
+        ],
+    },
+    {
+        "job_title": "Enseignant vacataire en Licence Pro",
+        "time": "Oct. 2012 - Aujourd'hui",
+        "company": "IUT de Sénart, Lieusaint",
+        "details": [
+            "Scripting (Python, Perl)",
+            "Chiffrement et certificats",
+            "VPN",
+        ],
+    },
+    {
+        "job_title": "Ingénieur Systèmes Linux",
+        "time": "Mai 2020 - Octobre 2020",
+        "company": "Université Gustave Eiffel, Champs-sur-Marne",
+        "details": [
+            "Déploiement de services (WireGuard, pfSense, etc.)",
             "Écriture d'un démon de logging pour WireGuard",
-            ],
-        },
+        ],
+    },
     {
-        'job_title': 'Enseignant vacataire en Licence Pro',
-        'time': "Oct. 2012 - Aujourd'hui",
-        'company': 'IUT de Sénart, Lieusaint',
-        'details': [
-            'Scripting (Python, Perl)',
-            'Chiffrement et certificats',
-            'VPN',
-            ],
-        },
-    {
-        'job_title': 'Ingénieur Systèmes Linux',
-        'time': 'Sep. 2017 - Avril 2020',
-        'company': 'UbiCast, Ivry-sur-Seine',
-        'details': [
+        "job_title": "Ingénieur Systèmes Linux",
+        "time": "Sep. 2017 - Avril 2020",
+        "company": "UbiCast, Ivry-sur-Seine",
+        "details": [
             "Déploiement d'infrastructure avec Terraform, Packer et Ansible",
             "Portage des scripts de déploiement (Python/Bash) vers Ansible",
             "Mise en haute-disponibilité du produit (cluster PostgreSQL, HAProxy, etc.)",
-            ],
-        },
+        ],
+    },
     {
-        'job_title': 'Responsable informatique',
-        'time': 'Sep. 2016 - Août 2017',
-        'company': 'Lycée Technique Privé Saint-Nicolas, Paris',
-        'details': [
+        "job_title": "Responsable informatique",
+        "time": "Sep. 2016 - Août 2017",
+        "company": "Lycée Technique Privé Saint-Nicolas, Paris",
+        "details": [
             "Développement d'un gestionnaire de comptes utilisateurs (Django)",
             "Migration serveur de messagerie interne (Postfix, Dovecot)",
             "Mise en place Nextcloud pour les professeurs et étudiants",
-            ],
-        },
+        ],
+    },
     {
-        'job_title': 'Administrateur Systèmes et Réseaux',
-        'time': 'Sep. 2015 - Sep. 2016',
-        'company': 'IUT de Sénart, Lieusaint',
-        'details': [
-            'Administration système (Debian/Ubuntu)',
-            'Développement (PHP/Bash/Python)',
-            'Exploitation de parc (Ubuntu/Windows)',
-            ],
-        },
+        "job_title": "Administrateur Systèmes et Réseaux",
+        "time": "Sep. 2015 - Sep. 2016",
+        "company": "IUT de Sénart, Lieusaint",
+        "details": [
+            "Administration système (Debian/Ubuntu)",
+            "Développement (PHP/Bash/Python)",
+            "Exploitation de parc (Ubuntu/Windows)",
+        ],
+    },
     {
-        'job_title': 'Administrateur Systèmes et Réseaux',
-        'time': 'Août 2011 - Déc. 2014',
-        'company': 'Lycée Technique Privé Saint-Nicolas, Paris',
-        'details': [
-            'Administration serveurs GNU/Linux et Windows',
-            'Conception nouvelle infrastructure : Virtualisation, VPN, Active Directory, etc.',
-            ],
-        },
+        "job_title": "Administrateur Systèmes et Réseaux",
+        "time": "Août 2011 - Déc. 2014",
+        "company": "Lycée Technique Privé Saint-Nicolas, Paris",
+        "details": [
+            "Administration serveurs GNU/Linux et Windows",
+            "Conception nouvelle infrastructure : Virtualisation, VPN, Active Directory, etc.",
+        ],
+    },
     {
-        'job_title': 'Informaticien HelpDesk',
-        'time': 'Août 2009 - Août 2011',
-        'company': "Châteaud'eau, La Courneuve",
-        'details': [
+        "job_title": "Informaticien HelpDesk",
+        "time": "Août 2009 - Août 2011",
+        "company": "Châteaud'eau, La Courneuve",
+        "details": [
             "Gestion, maintenance, installation (Serveurs & PC)",
             "Participation à l'implémentation de projets d'infrastructure",
             "Pilotage de la migration de l'ancien logiciel client ERP vers le nouveau",
-            ]
-        },
-    ]
+        ],
+    },
+]
 
 EDUCATIONS = [
     {
-        'degree': 'Licence Pro Administration Systèmes et Réseaux',
-        'meta': 'IUT de Sénart, Lieusaint',
-        'time': 'Oct. 2011 - Juin 2012',
-        },
+        "degree": "Licence Pro Administration Systèmes et Réseaux",
+        "meta": "IUT de Sénart, Lieusaint",
+        "time": "Oct. 2011 - Juin 2012",
+    },
     {
-        'degree': 'BTS Informatique de Gestion',
-        'meta': 'Institut Medicis Alternance, Paris',
-        'time': 'Sep. 2009 - Mai 2011',
-        },
+        "degree": "BTS Informatique de Gestion",
+        "meta": "Institut Medicis Alternance, Paris",
+        "time": "Sep. 2009 - Mai 2011",
+    },
     {
-        'degree': 'Bac Pro Micro-informatique et Réseaux',
-        'meta': 'Lycée Jacques Prevert, Combs-la-Ville',
-        'time': 'Sep. 2007 - Juin 2009',
-        },
-    ]
+        "degree": "Bac Pro Micro-informatique et Réseaux",
+        "meta": "Lycée Jacques Prevert, Combs-la-Ville",
+        "time": "Sep. 2007 - Juin 2009",
+    },
+]
